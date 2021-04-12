97cddc9a3b521590ea3f3eece31f65b3f033836c
Octavian Popescu <daniel.popescu0903@stud.acs.upb.ro>
2021-04-12 11:02:26 +0300

    solved C

--- Colocviu1_2/app/src/main/java/ro/pub/cs/systems/eim/Colocviu1_2/Colocviu1_2MainActivity.java
+++ Colocviu1_2/app/src/main/java/ro/pub/cs/systems/eim/Colocviu1_2/Colocviu1_2MainActivity.java
@@ -1,12 +1,16 @@
 package ro.pub.cs.systems.eim.Colocviu1_2;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import org.w3c.dom.Text;
 
@@ -15,6 +19,9 @@ public class Colocviu1_2MainActivity extends AppCompatActivity {
     private TextView allTermsTextView;
     private Button addButton;
     private Button computeButton;
+
+    private int result = 0;
+    private String lastCallAllTermsString = "";
 
     private class AddButtonListener implements View.OnClickListener {
         @Override
@@ -41,6 +48,25 @@ public class Colocviu1_2MainActivity extends AppCompatActivity {
 
     private AddButtonListener addButtonListener = new AddButtonListener();
 
+    private class ComputeButtonListener implements View.OnClickListener {
+        @Override
+        public void onClick(View v) {
+            String allTermsString = allTermsTextView.getText().toString();
+
+            if (lastCallAllTermsString.isEmpty() || !lastCallAllTermsString.equals(allTermsString)) {
+                lastCallAllTermsString = allTermsString;
+
+                Intent intent = new Intent(getApplicationContext(), Colocviu1_2SecondaryActivity.class);
+                intent.putExtra(Constants.ALL_TERMS, allTermsString);
+                startActivityForResult(intent, Constants.REQUEST_CODE);
+            } else {
+                Toast.makeText(getApplicationContext(), "Computed value stored in activity is: " + result, Toast.LENGTH_LONG).show();
+            }
+        }
+    }
+
+    private ComputeButtonListener computeButtonListener = new ComputeButtonListener();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -53,5 +79,28 @@ public class Colocviu1_2MainActivity extends AppCompatActivity {
         addButton.setOnClickListener(addButtonListener);
 
         computeButton = findViewById(R.id.compute_button);
+        computeButton.setOnClickListener(computeButtonListener);
+
+        if (savedInstanceState != null) {
+            if (savedInstanceState.containsKey(Constants.SAVED_RESULT)) {
+                result = savedInstanceState.getInt(Constants.SAVED_RESULT);
+            }
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == Constants.REQUEST_CODE) {
+            Toast.makeText(this, "Computed value by secondary activity is: " + resultCode, Toast.LENGTH_LONG).show();
+            result = resultCode;
+        }
+    }
+
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+
+        outState.putInt(Constants.SAVED_RESULT, result);
     }
 }
--- Colocviu1_2/app/src/main/java/ro/pub/cs/systems/eim/Colocviu1_2/Constants.java
+++ Colocviu1_2/app/src/main/java/ro/pub/cs/systems/eim/Colocviu1_2/Constants.java
@@ -2,4 +2,6 @@ package ro.pub.cs.systems.eim.Colocviu1_2;
 
 public class Constants {
     public static final String ALL_TERMS = "all_terms";
+    public static final int REQUEST_CODE = 2021;
+    public static final String SAVED_RESULT = "saved_result";
 }
